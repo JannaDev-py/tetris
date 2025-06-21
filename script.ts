f11e8b14b3d804ae2c22aa24e1eee0edb4e5232b
JannaDev-py <orlandojannael@gmail.com>
2025-06-21 10:43:24 -0600

    pieces color

--- script.ts
+++ script.ts
@@ -34,24 +34,39 @@ const pieces = [
     },
     color: 'red',
     rotate: 0
+  },
+  // [][][]
+  {
+    cordinates: [
+      { x: 0, y: 0 },
+      { x: 1, y: 0 },
+      { x: 2, y: 0 }
+    ],
+    position: {
+      x: 0,
+      y: 0
+    },
+    color: '#f21',
+    rotate: 0
   }
 ]
 
-const gameboard: Array<Array<Number | typeof pieces[0]>> = Array.from({ length: config.height }, () => Array(config.width).fill(0))
+const gameboard: Array<Array<Number | typeof pieces[1]>> = Array.from({ length: config.height }, () => Array(config.width).fill(0))
 
-gameboard[0][0] = pieces[0]
-gameboard[pieces[0].cordinates[0].y][pieces[0].cordinates[0].x] = 1
-gameboard[pieces[0].cordinates[1].y][pieces[0].cordinates[1].x] = 1
-gameboard[pieces[0].cordinates[2].y][pieces[0].cordinates[2].x] = 1
-gameboard[pieces[0].cordinates[3].y][pieces[0].cordinates[3].x] = 1
+gameboard[0][0] = pieces[1]
+gameboard[pieces[1].cordinates[0].y][pieces[1].cordinates[0].x] = pieces[1]
+gameboard[pieces[1].cordinates[1].y][pieces[1].cordinates[1].x] = pieces[1]
+gameboard[pieces[1].cordinates[2].y][pieces[1].cordinates[2].x] = pieces[1]
 
 function renderGameboard (): void {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   gameboard.forEach((row, y) => {
     row.forEach((cell, x) => {
       if (cell !== 0) {
-        ctx.fillStyle = (cell as typeof pieces[0]).color
+        ctx.beginPath()
+        ctx.fillStyle = (cell as typeof pieces[1]).color
         ctx.fillRect(x * config.pieceWidth, y * config.pieceHeight, config.pieceWidth, config.pieceHeight)
+        ctx.closePath()
       }
     })
   })
