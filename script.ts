0beae6052aaf02d0d0cd9d7df74b4fba017c2ce1
JannaDev-py <orlandojannael@gmail.com>
2025-06-21 11:30:29 -0600

    generate piece fn

--- script.ts
+++ script.ts
@@ -8,6 +8,9 @@ canvas.height = canvasContainer.clientHeight
 window.addEventListener('resize', () => {
   canvas.width = canvasContainer.clientWidth
   canvas.height = canvasContainer.clientHeight
+  config.pieceWidth = canvas.width / 20
+  config.pieceHeight = canvas.height / 30
+  renderGameboard()
 })
 
 const config = {
@@ -15,6 +18,15 @@ const config = {
   height: 30,
   pieceWidth: canvas.width / 20,
   pieceHeight: canvas.height / 30
+}
+
+interface Piece {
+  cordinates: Array<{ x: number, y: number }>
+  position: { x: number, y: number }
+  color: string
+  rotate: number
+  width: number
+  height: number
 }
 
 // all pieces will start from left to right, top to bottom
@@ -32,8 +44,10 @@ const pieces = [
       x: 0,
       y: 0
     },
-    color: 'red',
-    rotate: 0
+    color: '#3f7',
+    rotate: 0,
+    width: 3,
+    height: 2
   },
   // [][][]
   {
@@ -47,16 +61,31 @@ const pieces = [
       y: 0
     },
     color: '#f21',
-    rotate: 0
+    rotate: 0,
+    width: 3,
+    height: 1
+  },
+  //   [][]
+  // [][]
+  {
+    cordinates: [
+      { x: 1, y: 0 },
+      { x: 2, y: 0 },
+      { x: 0, y: 1 },
+      { x: 1, y: 1 }
+    ],
+    position: {
+      x: 0,
+      y: 0
+    },
+    color: '#f21',
+    rotate: 0,
+    width: 3,
+    height: 2
   }
 ]
 
-const gameboard: Array<Array<Number | typeof pieces[1]>> = Array.from({ length: config.height }, () => Array(config.width).fill(0))
-
-gameboard[0][0] = pieces[1]
-gameboard[pieces[1].cordinates[0].y][pieces[1].cordinates[0].x] = pieces[1]
-gameboard[pieces[1].cordinates[1].y][pieces[1].cordinates[1].x] = pieces[1]
-gameboard[pieces[1].cordinates[2].y][pieces[1].cordinates[2].x] = pieces[1]
+const gameboard: Array<Array<Number | Piece>> = Array.from({ length: config.height }, () => Array(config.width).fill(0))
 
 function renderGameboard (): void {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
@@ -64,7 +93,7 @@ function renderGameboard (): void {
     row.forEach((cell, x) => {
       if (cell !== 0) {
         ctx.beginPath()
-        ctx.fillStyle = (cell as typeof pieces[1]).color
+        ctx.fillStyle = (cell as Piece).color
         ctx.fillRect(x * config.pieceWidth, y * config.pieceHeight, config.pieceWidth, config.pieceHeight)
         ctx.closePath()
       }
@@ -72,4 +101,28 @@ function renderGameboard (): void {
   })
 }
 
-renderGameboard()
+function generatePiece (): void {
+  const randomPiece = { ...pieces[Math.floor(Math.random() * pieces.length)] }
+  let randomX = Math.floor(Math.random() * (config.width - randomPiece.width))
+  randomPiece.color = '#' + Math.floor(Math.random() * 16777215).toString(16)
+
+  while (gameboard[0][randomX] !== 0) {
+    randomX = Math.floor(Math.random() * (config.width - randomPiece.width))
+  }
+
+  // lets put the piece at the positions it needs to be
+  randomPiece.position.x = randomX
+  randomPiece.cordinates.forEach((cordinate) => {
+    gameboard[cordinate.y][cordinate.x + randomPiece.position.x] = randomPiece
+  })
+  renderGameboard()
+}
+
+function initGame (): void {
+}
+
+generatePiece()
+
+setInterval(() => {
+  initGame()
+}, 1000)
